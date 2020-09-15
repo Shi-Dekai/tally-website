2f9a24620589137c87eb73a336d14f8693068069
shi <1727259503@qq.com>
2020-09-15 16:29:37 +0800

    deploy

---
+++ precache-manifest.e71cc1de6b45e105066fcd2cee8c6a39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ef3a535b66df345d8a",
+    "url": "/tally-website/css/app.9d445988.css"
+  },
+  {
+    "revision": "abbb4f38f7f23124cd47d144b29fc99b",
+    "url": "/tally-website/index.html"
+  },
+  {
+    "revision": "c0ef3a535b66df345d8a",
+    "url": "/tally-website/js/app.b11c2b2c.js"
+  },
+  {
+    "revision": "abd4ecea05966f7cf05c",
+    "url": "/tally-website/js/chunk-vendors.da774917.js"
+  },
+  {
+    "revision": "3b3ac03b730001df064d3e92befbe4e5",
+    "url": "/tally-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-website/robots.txt"
+  },
+  {
+    "revision": "95f917edd22d729cf9d96dbfe68a519f",
+    "url": "/tally-website/sps-title.ico"
+  }
+]);
